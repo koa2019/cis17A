f061ab7fc5cf31a3f0d7d3545fe790bd434c649b
Fernandez <dfernandez10@student.rccd.edu>
2022-09-01 14:52:10 -0700

    added introLab and labConversion
    Signed-off-by: Fernandez <dfernandez10@student.rccd.edu>

---
+++ lab/lab/introLab/helloWorld/main.cpp
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Danielle Fernandez
+ * CSC 17A *
+ * Created on September 1, 2022, 2:20 PM
+ * Hello World
+ */
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    cout << "Hello World"<<endl;
+    
+    return 0;
+}
+
